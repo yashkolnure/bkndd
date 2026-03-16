201d21a04d6993200409cf6eb9545bd81a4b8984
unknown <yashkolnure58@gmail.com>
2026-03-16 15:01:33 +0530

    Initial commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/integrations', require('./routes/integrations'));
 
 
 /* ---------- Server ---------- */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 const server = app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server running on http://127.0.0.1:${PORT}`);
