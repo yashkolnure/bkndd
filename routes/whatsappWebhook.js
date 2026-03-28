9ad0c03d71808b4b15c59758ce8c32f16e4d73cb
unknown <yashkolnure58@gmail.com>
2026-03-29 02:01:41 +0530

    Initial commit

--- routes/whatsappWebhook.js
+++ routes/whatsappWebhook.js
@@ -9,6 +9,20 @@ const ProcessedMessage = require("../models/ProcessedMessage");
 const { sendPushNotification } = require("../lib/firebase");
 
 const userLocks = new Map();
+const VERIFY_TOKEN = 'ma_wa_handshake_kyifcljsxujudsjnxavenirya2026';
+
+app.get('/webhook', (req, res) => {
+  const mode      = req.query['hub.mode'];
+  const token     = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 
 router.post("/whatsapp-test", async (req, res) => {
   const body = req.body;
